98b43149ac1e6397ae5ea0e22057fd03b1d91725
anna.v.glazunova@yandex.ru <AnnaGlazunova>
2022-12-23 12:50:20 +0300

    Task 34 done

---
+++ Task34/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] array = new int[10];
+array[0] = new Random().Next(100, 1000);
+Console.Write("[" + array[0]);
+
+for (int i = 1; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.Write(", " + array[i]);
+}
+Console.Write("]");
+Console.WriteLine("");
+int countEven = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2==0)
+    {
+        countEven += 1;
+    }
+}
+Console.WriteLine("Number of even numbers: " + countEven);
